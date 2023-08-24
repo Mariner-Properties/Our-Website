eafaeb204aaeadc354af14a8ded8c399c32b74aa
Ryan <marinerpropertiesrental@gamil.com>
2023-08-24 17:09:12 -0400

    Revamped Applications

---
+++ applicationassets/script.js
@@ -0,0 +1,5 @@
+function email() {
+    var link = "mailto:marinerpropertiesrental@gmail.com"
+    + "&body=" + encodeURIComponent(document.getElementById('C:\Users\rpgil\OneDrive\Documents\App 4 Tenancy New (Fillable).pdf').value)
+    ;
+}
