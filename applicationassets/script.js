91f8e74d81a66c4bfa97af32dbf90bf2b13ab0b7
Ryan <marinerpropertiesrental@gamil.com>
2023-08-26 12:26:25 -0400

    Adding Photos to Repo

--- applicationassets/script.js
+++ applicationassets/script.js
@@ -1,5 +1,4 @@
 function email() {
-    var link = "mailto:marinerpropertiesrental@gmail.com"
-    + "&body=" + encodeURIComponent(document.getElementById('C:\Users\rpgil\OneDrive\Documents\App 4 Tenancy New (Fillable).pdf').value)
-    ;
+    getelementbyname=
+    var link = "mailto:marinerpropertiesrental@gmail.com";
 }
