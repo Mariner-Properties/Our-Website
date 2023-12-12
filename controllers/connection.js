f47cfa960867b0bfa25e9f5f7b74fecc0a4f9365
Ryan <marinerpropertiesrental@gamil.com>
2023-12-11 19:41:56 -0500

    jawsdb try 7

--- controllers/connection.js
+++ controllers/connection.js
@@ -10,27 +10,27 @@ if (process.env.JAWSDB_URL) {
   sequelize2 = new Sequelize(process.env.JAWSDB_URL);
 } else {
   // If JAWSDB_URL is not available, use local configurations for sequelize and sequelize2
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+  // sequelize = new Sequelize(
+  //   process.env.DB_NAME,
+  //   process.env.DB_USER,
+  //   process.env.DB_PASSWORD,
+  //   {
+  //     host: 'localhost',
+  //     dialect: 'mysql',
+  //     port: 3306
+  //   }
+  // );
 
-  sequelize2 = new Sequelize(
-    process.env.DB_NAME_2,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+  // sequelize2 = new Sequelize(
+  //   process.env.DB_NAME_2,
+  //   process.env.DB_USER,
+  //   process.env.DB_PASSWORD,
+  //   {
+  //     host: 'localhost',
+  //     dialect: 'mysql',
+  //     port: 3306
+  //   }
+  // );
 }
 
 module.exports = {
