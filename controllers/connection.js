ff4062923cde11f23c8a530064a7dfaca886dae1
Ryan <marinerpropertiesrental@gamil.com>
2023-12-11 19:48:01 -0500

    debugging

--- controllers/connection.js
+++ controllers/connection.js
@@ -2,38 +2,14 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 let sequelize;
-let sequelize2;
 
 if (process.env.JAWSDB_URL) {
-  // If JAWSDB_URL is available, use it for both sequelize and sequelize2
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-  sequelize2 = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  // If JAWSDB_URL is not available, use local configurations for sequelize and sequelize2
-  // sequelize = new Sequelize(
-  //   process.env.DB_NAME,
-  //   process.env.DB_USER,
-  //   process.env.DB_PASSWORD,
-  //   {
-  //     host: 'localhost',
-  //     dialect: 'mysql',
-  //     port: 3306
-  //   }
-  // );
-
-  // sequelize2 = new Sequelize(
-  //   process.env.DB_NAME_2,
-  //   process.env.DB_USER,
-  //   process.env.DB_PASSWORD,
-  //   {
-  //     host: 'localhost',
-  //     dialect: 'mysql',
-  //     port: 3306
-  //   }
-  // );
+  console.error('JAWSDB_URL is not set in environment variables');
+  // Handle the error or exit the application
 }
 
 module.exports = {
-  sequelize: sequelize,
-  sequelize2: sequelize2
+  sequelize: sequelize
 };
